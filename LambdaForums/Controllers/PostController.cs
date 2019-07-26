3115f0f7e202d1b1cc2bb02196d4384e9325f7b1
rtitvilasi <40577984+rtitvilasi@users.noreply.github.com>
2019-07-26 15:07:32 +0530

    version3

--- LambdaForums/Controllers/PostController.cs
+++ LambdaForums/Controllers/PostController.cs
@@ -6,6 +6,7 @@ using LambdaForums.Data;
 using LambdaForums.Data.Models;
 using LambdaForums.Models.Post;
 using LambdaForums.Models.Reply;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LambdaForums.Controllers
@@ -13,9 +14,14 @@ namespace LambdaForums.Controllers
     public class PostController : Controller
     {
         private readonly IPost _postService;
-        public PostController(IPost postService)
+        private readonly IForum _forumService;
+
+        private static UserManager<ApplicationUser> _userManager;
+        public PostController(IPost postService, IForum forumService, UserManager<ApplicationUser> userManager)
         {
             _postService = postService;
+            _forumService = forumService;
+            _userManager = userManager;
         }
 
         public IActionResult Index(int id)
@@ -38,7 +44,45 @@ namespace LambdaForums.Controllers
 
             return View(model);
         }
+        public IActionResult Create(int id)
+        {
+            var forum = _forumService.GetById(id);
+            var model = new NewPostModel
+            {
+                ForumName = forum.Title,
+                ForumId=forum.Id,
+                ForumImageUrl=forum.ImageUrl,
+                AuthorName=User.Identity.Name
+            };
+            return View(model);
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> AddPost(NewPostModel model)
+        {
+            var userId = _userManager.GetUserId(User);
+            var user =   _userManager.FindByIdAsync(userId).Result;
+            var post = BuildPost(model, user);
+
+             _postService.Add(post).Wait();  //this line blocks the current thread ntyil all the task is complete.
+            // TODO:USER RATING MANAGEMENT HERE
+
+            return RedirectToAction("Index", "Post",new { id = post.Id });
+
+        }
+
+        private Post BuildPost(NewPostModel model, ApplicationUser user)
+        {
+            var forum = _forumService.GetById(model.ForumId);
+            return new Post
+            {
+                Title = model.Title,
+                Content = model.Content,
+                Created = DateTime.Now,
+                User = user,
+                Forum=forum
+            };
+        }
 
         private IEnumerable<PostReplyModel> BuildPostReplies(IEnumerable<PostReply> replies)
         {
